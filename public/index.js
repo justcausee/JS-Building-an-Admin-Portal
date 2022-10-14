c619837e9820a390fc6db17ad2ce449df7d622f8
Alan <alannhan@Alans-MacBook-Air.local>
2022-10-13 20:23:32 -0700

    Tesing

--- public/index.js
+++ public/index.js
@@ -26,3 +26,16 @@ function renderBook(book) {
 }
 
 main()
+
+let response = await fetch('http://localhost:9001/updateBook', {
+    method: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }), 
+});
+let updatedBook = await response.json();
+console.log(updatedBook)
